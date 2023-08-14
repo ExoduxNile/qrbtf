e49267042febb83693666b4aedae4da3046a0a2a
ExoduxNile <sandeep.tete2012@gmail.com>
2023-08-14 13:02:15 +0530

    Update PartDownload.js

--- src/components/app/PartDownload.js
+++ src/components/app/PartDownload.js
@@ -103,8 +103,15 @@ const PartDownload = ({ value, downloadCount, onSvgDownload, onImgDownload }) =>
                     <button className="dl-btn" onClick={() => {onImgDownload("jpg").then(res => setImgData(res));}}>JPG</button>                    
                     <button className="dl-btn" onClick={() => {onImgDownload("png").then(res => setImgData(res));}}>PNG</button>
                     <button className="dl-btn" onClick={onSvgDownload}>SVG</button>
-                    <button className="dl-btn" style={{ backgroundColor: '#9900EF' }} onClick={formData}>SHOW</button>
-                </div>
+<button onClick={async () => {
+  
+  try {
+    await onImgDownload();
+  } catch (error) {
+    console.log(error);
+  }
+
+}}>SHOW</button>                </div>
             </div>
             <div id="wx-message">
                 <WxMessage/>
