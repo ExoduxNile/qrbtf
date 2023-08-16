ee8a739baf537c04863693a08dd423ab2e1263ee
ExoduxNile <sandeep.tete2012@gmail.com>
2023-08-16 15:16:20 +0530

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,7 +7,6 @@ import PartMore from "./PartMore";
 import PartParams from "./PartParams";
 import PartDownloadViewer from "../../containers/app/PartDownloadViewer";
 import PartStylesViewer from "../../containers/app/PartStylesViewer";
-//import {getDownloadCount, login} from "../../api/TcbHandler";
 import {connect} from 'react-redux';
 import {loadDownloadData} from "../../actions";
 import ReactGA from 'react-ga';
@@ -17,12 +16,7 @@ ReactGA.initialize('G-N6SB8GPHLZ');
 
 ReactGA.addTrackers(
     [
-        /*{
-            trackingId: 'UA-165845289-1',
-            gaOptions: {
-                name: 'trackerUA',
-            }
-        },*/
+        
         {
             trackingId: 'G-N6SB8GPHLZ',
             gaOptions: { name: 'trackerG' }
@@ -32,37 +26,39 @@ ReactGA.addTrackers(
 );
 
 function App({ dispatch }) {
-    const updateDownloadData = useCallback((downloadData) => dispatch(loadDownloadData(downloadData)), []);
-    setScrollbarWidthProp()
 
-    useEffect(() => {
-        //login().then(() => {
-            //getDownloadCount((res) => {
-                let downloadData = [];
-                forEach((item) => {
-                    downloadData[item.value] = item.count;
-                });
-                dispatch(loadDownloadData(downloadData));
-            //});
-        //})
-    })
+  const updateDownloadData = useCallback(
+    (downloadData) => dispatch(loadDownloadData(downloadData)), 
+    [dispatch] // dispatch should be added to dependencies
+  );
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <div className="Layout">
-                    <div className="Qr-outer">
-                        <PartHeader/>
-                        <PartStylesViewer/>
-                        <PartParams/>
-                        <PartDownloadViewer updateDownloadData={updateDownloadData}/>
-                        <PartMore/>
-                        <PartFooter/>
-                    </div>
-                </div>
-            </header>
+  useEffect(() => {
+    let downloadData = [];
+
+    forEach(item => {
+      downloadData[item.value] = item.count; 
+    });
+
+    dispatch(loadDownloadData(downloadData));
+
+  }, [dispatch, downloadData]) // dispatch and downloadData should be added to dependencies
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Layout">
+          <div className="Qr-outer">
+            <PartHeader/>
+            <PartStylesViewer/>
+            <PartParams/>
+            <PartDownloadViewer updateDownloadData={updateDownloadData}/> 
+            <PartMore/>
+            <PartFooter/>
+          </div>
         </div>
-    );
+      </header>
+    </div>
+  );
 }
 
 export default connect()(App);
