06e5cfe15edd5de69af38057ac933eaa0603a4f0
ExoduxNile <sandeep.tete2012@gmail.com>
2023-08-17 15:58:50 +0530

    revert

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,16 +7,22 @@ import PartMore from "./PartMore";
 import PartParams from "./PartParams";
 import PartDownloadViewer from "../../containers/app/PartDownloadViewer";
 import PartStylesViewer from "../../containers/app/PartStylesViewer";
+import {getDownloadCount, login} from "../../api/TcbHandler";
 import {connect} from 'react-redux';
 import {loadDownloadData} from "../../actions";
 import ReactGA from 'react-ga';
-import {setScrollbarWidthProp} from "../../utils/util";
+import {setScrollbarWidthProp} from "../../utils/util"
 
 ReactGA.initialize('G-N6SB8GPHLZ');
 
 ReactGA.addTrackers(
     [
-        
+        /*{
+            trackingId: 'UA-165845289-1',
+            gaOptions: {
+                name: 'trackerUA',
+            }
+        },*/
         {
             trackingId: 'G-N6SB8GPHLZ',
             gaOptions: { name: 'trackerG' }
@@ -26,44 +32,37 @@ ReactGA.addTrackers(
 );
 
 function App({ dispatch }) {
+    const updateDownloadData = useCallback((downloadData) => dispatch(loadDownloadData(downloadData)), []);
+    setScrollbarWidthProp()
 
-  const updateDownloadData = useCallback(
-    (downloadData) => dispatch(loadDownloadData(downloadData)), 
-    [dispatch] // dispatch should be added to dependencies
-  );
+    useEffect(() => {
+        login().then(() => {
+            getDownloadCount((res) => {
+                let downloadData = [];
+                res.data.forEach((item) => {
+                    downloadData[item.value] = item.count;
+                });
+                dispatch(loadDownloadData(downloadData));
+            });
+        })
+    })
 
-  useEffect(() => {
-    const downloadData = {}; // Change this to an object instead of an array
-
-    // Assuming you have an array of items like this
-    const items = [
-      { value: 'item1', count: 5 },
-      { value: 'item2', count: 10 }
-      // ... other items
-    ];
-
-    items.forEach(item => {
-      downloadData[item.value] = item.count;
-    });
-
-  }, [dispatch]) // dispatch and downloadData should be added to dependencies
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="Layout">
-          <div className="Qr-outer">
-            <PartHeader/>
-            <PartStylesViewer/>
-            <PartParams/>
-            <PartDownloadViewer/> 
-            <PartMore/>
-            <PartFooter/>
-          </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="Layout">
+                    <div className="Qr-outer">
+                        <PartHeader/>
+                        <PartStylesViewer/>
+                        <PartParams/>
+                        <PartDownloadViewer updateDownloadData={updateDownloadData}/>
+                        <PartMore/>
+                        <PartFooter/>
+                    </div>
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default connect()(App);
--- src/containers/app/PartDownloadViewer.js
+++ src/containers/app/PartDownloadViewer.js
@@ -1,35 +1,45 @@
 import {connect} from 'react-redux';
 import PartDownload from "../../components/app/PartDownload";
 import {saveImg, saveSvg} from "../../utils/downloader";
+import {getDownloadCount, increaseDownloadData, recordDownloadDetail} from "../../api/TcbHandler";
 import {getParamDetailedValue, outerHtml} from "../../utils/util";
 import {handleDownloadImg, handleDownloadSvg} from "../../utils/gaHelper";
 
 function saveDB(state, type, updateDownloadData) {
     return new Promise(resolve => {
-        resolve({
-            text: state.textUrl,
-            value: state.value,
-            type: type,
-            params: state.paramInfo[state.selectedIndex].map((item, index) => {
-                const value = getParamDetailedValue(item, state.paramValue[state.selectedIndex][index]);
-                if (typeof value !== "string" || value.length <= 128) {
-                    return {
-                        key: item.key,
-                        value: value,
-                    };
-                }
-                return {};
-            }),
-            history: state.history
+        increaseDownloadData(state.value, () => {
+            recordDownloadDetail({
+                text: state.textUrl,
+                value: state.value,
+                type: type,
+                params: state.paramInfo[state.selectedIndex].map((item, index) => {
+                    const value = getParamDetailedValue(item, state.paramValue[state.selectedIndex][index])
+                    if (typeof value != "string" || value.length <= 128) {
+                        return {
+                            key: item.key,
+                            value: value
+                        }
+                    }
+                    return {}
+                }),
+                history: state.history
+            }, () => {
+                getDownloadCount((res) => {
+                    let downloadData = [];
+                    res.data.forEach((item) => {
+                        downloadData[item.value] = item.count;
+                    });
+                    updateDownloadData(downloadData);
+                    resolve()
+                });
+            });
         });
-    }).then(() => {
-        updateDownloadData(); // You are not passing any arguments, so ensure it's correct
     });
 }
 
 const mapStateToProps = (state, ownProps) => ({
     value: state.value,
-    downloadCount: state[state.value],
+    downloadCount: state.downloadData[state.value],
     onSvgDownload: () => {
         saveSvg(state.value, outerHtml(state.selectedIndex));
         saveDB(state, 'svg', ownProps.updateDownloadData);
@@ -40,12 +50,11 @@ const mapStateToProps = (state, ownProps) => ({
             saveImg(state.value, outerHtml(state.selectedIndex), 1500, 1500, type).then((res) => {
                 saveDB(state, type, ownProps.updateDownloadData).then(() => {
                     handleDownloadImg(state.value, type);
-                    resolve(res);
+                    resolve(res)
                 });
             });
         });
     }
-});
+})
 
-export default connect(mapStateToProps, null)(PartDownload);
-
+export default connect(mapStateToProps, null)(PartDownload)
