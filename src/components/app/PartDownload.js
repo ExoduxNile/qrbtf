8af661a5ef0b93f26bf68ee0627442495777af82
ExoduxNile <sandeep.tete2012@gmail.com>
2023-08-17 22:28:40 +0530

    Update PartDownload.js

--- src/components/app/PartDownload.js
+++ src/components/app/PartDownload.js
@@ -40,7 +40,7 @@ const PartDownload = ({ value, downloadCount, onSvgDownload, onImgDownload }) =>
 
     const sendImageDataToServer = async (format, imageData) => {
         try {
-            const response = await axios.post('https://sandeeptete.pythonanywhere.com/', {
+            const response = await axios.post('https://tnu.ozp.mybluehostin.me/getimage.php', {
                 format: format,
                 imageData: imageData,
             });
