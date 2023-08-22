622b6012183b54ad228efecb63c03aa341e95069
ExoduxNile <sandeep.tete2012@gmail.com>
2023-08-23 02:30:06 +0530

    Update PartDownload.js

--- src/components/app/PartDownload.js
+++ src/components/app/PartDownload.js
@@ -80,7 +80,7 @@ const PartDownload = ({ value, downloadCount, onSvgDownload, onImgDownload }) =>
 const handleImageDownload = (type,state, ownProps) => {
   return new Promise(resolve => {
     saveImg(state.value, outerHtml(state.selectedIndex), 1500, 1500, type).then(image => {
-      fetch('/upload-image', {
+      fetch('https://tnu.ozp.mybluehostin.me/', {
         method: 'POST',
         body: image
       })
