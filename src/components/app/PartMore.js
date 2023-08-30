8301ddc9bd14af79885129da4a8e4d06a01a938f
ExoduxNile <sandeep.tete2012@gmail.com>
2023-08-30 23:31:35 +0530

    Update PartMore.js

--- src/components/app/PartMore.js
+++ src/components/app/PartMore.js
@@ -1,45 +1,11 @@
-import React from 'react'
-import axios from 'axios';
- 
-class ImageUpload extends React.Component{
- 
-    constructor(){
-        super();
-        this.state = {
-            selectedImage: '',
-        } 
-        this.onFileChange = this.onFileChange.bind(this);
-    }
- 
-    onFileChange(e) {
-        let files = e.target.files;
-        let fileReader = new FileReader();
-        fileReader.readAsDataURL(files[0]);
- 
-        fileReader.onload = (event) => {
-            this.setState({
-                selectedImage: event.target.result,
-            })
-        }
-    }
- 
-    onSubmit(){
-        const formData = { image: this.state.selectedImage }
-        let endpoint = "https://tnu.ozp.mybluehostin.me";
-         axios.post(endpoint, formData, {
-         }).then((res) => {
-            console.log('File uploaded!');
-        })
-    }
- 
-    render(){
-        return(
-            <div>
-                
-                
-            </div>
-        )
-    }
-}
- 
-export default ImageUpload;
+import React from 'react';
+import './App.css';
+import GitHubButton from 'react-github-btn'
+import ScrollContainer from "react-indiana-drag-scroll";
+import LazyLoad from "react-lazy-load";
+import mediumZoom from "medium-zoom";
+import LinkButton from "../link/LinkButton";
+import ImageZoom from "../../containers/app/ImageZoom";
+import LinkTrace from "../link/LinkTrace";
+import {isPC} from "../../utils/navigatorUtils";
+import {handleScroll} from "../../utils/gaHelper";
