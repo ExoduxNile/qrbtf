8555a51d230a826e706d08ff571cc1a6fb6aa55e
ExoduxNile <sandeep.tete2012@gmail.com>
2023-09-08 09:02:17 +0530

    Create scraper.js

---
+++ src/components/googlemaps/scraper.js
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import puppeteer from 'puppeteer';
+
+const getMapsData = async () => {     
+        browser = await puppeteer.launch({
+            headless: false,
+            args: ["--disabled-setuid-sandbox", "--no-sandbox"],
+        });
+        const page = await browser.newPage();
+        await page.setExtraHTTPHeaders({
+            "User-Agent":
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4882.194 Safari/537.36",
+        })
+        
+        
+        await page.goto("https://www.google.com/maps/search/Starbucks/@26.8484046,75.7215344,12z/data=!3m1!4b1" , {
+            waitUntil: 'domcontentloaded',
+            timeout: 60000
+        })
+        
+        await page.waitForTimeout(3000);
+        
+        let data =  await scrollPage(page,".m6QErb[aria-label]",2)
+        
+        console.log(data)
+        await browser.close();
+        };
+
+        const extractItems = async(page)  => {
+        let maps_data = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".Nv2PK")).map((el) => {
+            const link = el.querySelector("a.hfpxzc").getAttribute("href");
+            return {
+            title: el.querySelector(".qBF1Pd")?.textContent.trim(),
+            avg_rating: el.querySelector(".MW4etd")?.textContent.trim(),
+            reviews: el.querySelector(".UY7F9")?.textContent.replace("(", "").replace(")", "").trim(),
+            address: el.querySelector(".W4Efsd:last-child > .W4Efsd:nth-of-type(1) > span:last-child")?.textContent.replaceAll("·", "").trim(),
+            description: el.querySelector(".W4Efsd:last-child > .W4Efsd:nth-of-type(2)")?.textContent.replace("·", "").trim(),
+            website: el.querySelector("a.lcr4fd")?.getAttribute("href"),
+            category: el.querySelector(".W4Efsd:last-child > .W4Efsd:nth-of-type(1) > span:first-child")?.textContent.replaceAll("·", "").trim(),
+            timings: el.querySelector(".W4Efsd:last-child > .W4Efsd:nth-of-type(3) > span:first-child")?.textContent.replaceAll("·", "").trim(),
+            phone_num: el.querySelector(".W4Efsd:last-child > .W4Efsd:nth-of-type(3) > span:last-child")?.textContent.replaceAll("·", "").trim(),
+            extra_services: el.querySelector(".qty3Ue")?.textContent.replaceAll("·", "").replaceAll("  ", " ").trim(),
+            latitude: link.split("!8m2!3d")[1].split("!4d")[0],
+            longitude: link.split("!4d")[1].split("!16s")[0],
+            link,
+            dataId: link.split("1s")[1].split("!8m")[0],
+            };
+        });
+        });
+        return maps_data;
+        }    
